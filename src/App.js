3c88516280b0c54bd1785f9ecdebc6a3b25b905e
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-10-31 16:49:41 +0100

    buttons & reducer switch case

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-  
+      <h1>Counter App</h1>
+      <button>+</button>
+      <button>-</button>
+      <button>C</button>
+      <button>set</button>
     </div>
   );
 }
---
+++ src/components/Reducer.js
@@ -0,0 +1,12 @@
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "clear":
+      return (state = "");
+    default:
+      throw new Error();
+  }
+};
