89f2fd9b543a7f9a6d64b5d668dcfacfbeb2eb03
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-11-02 16:05:55 +0100

    ErrorBoundary & PageNotFound component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Counter from "./components/Counter";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Counter />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </main>
