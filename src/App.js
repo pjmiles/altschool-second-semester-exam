ec109b70ad795494e70e558d107163bf78417d45
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-11-02 15:44:46 +0100

    updated package.json file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import Counter from './components/Counter';
-import './App.css';
+import Counter from "./components/Counter";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <main className="App">
-     <Counter />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Counter />} />
+        </Routes>
+      </BrowserRouter>
     </main>
   );
 }
