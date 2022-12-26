969ca9bdcb89a99ac6d2352aff272dca46ebaf47
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-12-26 12:55:38 +0100

    decrement condition adjusted inside reducer

--- src/hook/Reducer.js
+++ src/hook/Reducer.js
@@ -5,7 +5,11 @@ export const reducer = (state, action) => {
     case "increment":
       return { ...state, count: state.count + 1 };
     case "decrement":
-      return { ...state, count: state.count - 1 };
+      if (state.count > 0) {
+        return { ...state, count: state.count - 1 };
+      } else {
+        return state;
+      }
     case "clear":
       return { ...state, count: (state.count = 0), userInput: action.payload };
     case "set":
